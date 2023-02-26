fc1a0083e7f0aa34bedc7c60b29acdce8aa43b0b
zrierc <68382685+zrierc@users.noreply.github.com>
2023-02-26 17:51:06 +0700

    Initial commit

---
+++ test/notes-api-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as NotesApiCdk from '../lib/notes-api-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/notes-api-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new NotesApiCdk.NotesApiCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
