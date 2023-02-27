7d678836eda7e726ee6e8710ff8fa451d6725b1b
zrierc <68382685+zrierc@users.noreply.github.com>
2023-02-27 10:50:14 +0700

    add role

--- lib/constructs/api-func-construct.ts
+++ lib/constructs/api-func-construct.ts
@@ -2,8 +2,16 @@ import { Construct } from 'constructs';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { RetentionDays } from 'aws-cdk-lib/aws-logs';
 import { Size } from 'aws-cdk-lib';
+import { IRole } from 'aws-cdk-lib/aws-iam';
 
 export class ApiLambdaProps {
+  /**
+   * Lambda execution role.
+   *
+   * This role will grant lambda function to get access to the database DynamoDB and write logs to CloudWatch Logs.
+   */
+  role: IRole;
+
   /**
    * The amount of memory, in MB, that is allocated to your Lambda function.
    *
@@ -32,13 +40,14 @@ export class ApiLambda extends Construct {
   constructor(scope: Construct, id: string, props: ApiLambdaProps) {
     super(scope, id);
 
-    const { memorySize, ephemeralStorageSize, logRetention } = props;
+    const { role, memorySize, ephemeralStorageSize, logRetention } = props;
 
     this.lambdaFunction = new NodejsFunction(this, 'ApiLambdaFunction', {
       functionName: 'notes-api-function',
       description: 'lambda function for notes API',
       ephemeralStorageSize: Size.mebibytes(ephemeralStorageSize!),
       memorySize,
+      role,
       logRetention,
     });
   }
