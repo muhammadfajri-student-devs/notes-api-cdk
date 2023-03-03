809a055d024fc33ec7113ab4f7296a9ae8ac3691
zrierc <68382685+zrierc@users.noreply.github.com>
2023-03-03 18:09:58 +0700

    define db

--- lib/notes-api-cdk-stack.ts
+++ lib/notes-api-cdk-stack.ts
@@ -1,10 +1,19 @@
 import * as cdk from 'aws-cdk-lib';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
+import { DynamoTable, DynamoTableProps } from './constructs/dynamodb-construct';
+
+export interface NotesApiCdkStackProps extends cdk.StackProps {
+  databaseConfigs: DynamoTableProps;
+}
 
 export class NotesApiCdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: NotesApiCdkStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const { databaseConfigs } = props;
+
+    // * Define Database
+    const dynamodb = new DynamoTable(this, 'DynamoDB', databaseConfigs);
   }
 }
