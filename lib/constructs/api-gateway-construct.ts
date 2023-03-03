56288d9d7be229c36cda5822e3f5291bb9d436a2
zrierc <68382685+zrierc@users.noreply.github.com>
2023-03-03 19:55:16 +0700

    define api gw

---
+++ lib/constructs/api-gateway-construct.ts
@@ -0,0 +1,77 @@
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import { IFunction } from 'aws-cdk-lib/aws-lambda';
+import { Construct } from 'constructs';
+
+export interface ApiGatewayProps {
+  /**
+   * The default Lambda function that handles all requests from this API.
+   */
+  handler: IFunction;
+
+  /**
+   * Configure a custom domain name and map it to this API.
+   */
+  domainName?: apigateway.DomainNameOptions;
+
+  /**
+   * Weather implement rate limit for API or not.
+   *
+   * @default false
+   */
+  enableRateLimitApi?: boolean;
+
+  /**
+   * The maximum number of requests that users can make within the specified time period.
+   *
+   * @default 10000
+   */
+  apiRequestLimit?: number;
+
+  /**
+   * The time period for which the maximum limit of requests applies.
+   *
+   * @default apigateway.Period.MONTH
+   */
+  apiRequestLimitPeriod?: apigateway.Period;
+}
+
+export class ApiGateway extends Construct {
+  public readonly lambdaRestApi: apigateway.LambdaRestApi;
+
+  constructor(scope: Construct, id: string, props: ApiGatewayProps) {
+    super(scope, id);
+
+    const {
+      handler,
+      domainName,
+      enableRateLimitApi,
+      apiRequestLimit,
+      apiRequestLimitPeriod,
+    } = props;
+
+    // * Define API Gateway
+    if (enableRateLimitApi) {
+      this.lambdaRestApi = new apigateway.LambdaRestApi(
+        this,
+        'LambdaApiGateway',
+        {
+          handler,
+          domainName,
+          cloudWatchRole: true,
+          deploy: true,
+          restApiName: 'notes-api-gateway',
+        }
+      );
+    }
+
+    // * Create rate limit for API
+    new apigateway.RateLimitedApiKey(this, 'LambdaApiRateLimit', {
+      apiKeyName: 'notes-api-gateway-key',
+      stages: [this.lambdaRestApi.deploymentStage],
+      quota: {
+        limit: apiRequestLimit || 10000,
+        period: apiRequestLimitPeriod || apigateway.Period.MONTH,
+      },
+    });
+  }
+}
--- lib/notes-api-cdk-stack.ts
+++ lib/notes-api-cdk-stack.ts
@@ -1,12 +1,28 @@
 import * as cdk from 'aws-cdk-lib';
 import * as iam from 'aws-cdk-lib/aws-iam';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import { Construct } from 'constructs';
 import { ApiLambda, ApiLambdaProps } from './constructs/api-func-construct';
 import { DynamoTable, DynamoTableProps } from './constructs/dynamodb-construct';
+import {
+  ApiGateway,
+  ApiGatewayProps,
+} from './constructs/api-gateway-construct';
 
 export interface NotesApiCdkStackProps extends cdk.StackProps {
+  /**
+   * Configuration for Lambda Function and Core Application
+   */
   lambdaFunctionConfigs: Omit<ApiLambdaProps, 'role'>;
 
+  /**
+   * Configuration for API Gateway
+   */
+  apiGatewayConfigs: Omit<ApiGatewayProps, 'handler' | 'domainName'>;
+
+  /**
+   * Configuration for DynamoDB Database
+   */
   databaseConfigs: DynamoTableProps;
 }
 
@@ -14,7 +30,7 @@ export class NotesApiCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: NotesApiCdkStackProps) {
     super(scope, id, props);
 
-    const { lambdaFunctionConfigs, databaseConfigs } = props;
+    const { lambdaFunctionConfigs, apiGatewayConfigs, databaseConfigs } = props;
 
     // * Define Database
     const dynamodb = new DynamoTable(this, 'DynamoDB', databaseConfigs);
@@ -44,12 +60,27 @@ export class NotesApiCdkStack extends cdk.Stack {
     lambdaRole.addToPolicy(dbPolicy);
 
     // * Define lambda function for notes API
-    const apiFunc = new ApiLambda(this, 'LambdaFunction', {
+    const lambda = new ApiLambda(this, 'LambdaFunction', {
       ...lambdaFunctionConfigs,
       role: lambdaRole,
       environment: {
         DYNAMODB_TABLE: databaseConfigs.tableName,
       },
     });
+
+    // * Define API Gateway for Lambda Function
+    const apiGw = new ApiGateway(this, 'ApiGateway', {
+      ...lambdaFunctionConfigs,
+      handler: lambda.lambdaFunction,
+    });
+
+    // set path and method that allowed
+    const notes = apiGw.lambdaRestApi.root.addResource('notes');
+    notes.addMethod('GET');
+    notes.addMethod('PUT');
+
+    const note = notes.addResource('{id}');
+    note.addMethod('GET');
+    note.addMethod('DELETE');
   }
 }
