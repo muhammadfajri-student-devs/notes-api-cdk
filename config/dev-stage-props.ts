c75d73b884614750ccaefb258f13a9997e81d689
muhammadfajri-student-devs <105767911+muhammadfajri-student-devs@users.noreply.github.com>
2023-03-05 17:25:19 +0700

    create configs

---
+++ config/dev-stage-props.ts
@@ -0,0 +1,37 @@
+import { RemovalPolicy } from 'aws-cdk-lib';
+import { Period } from 'aws-cdk-lib/aws-apigateway';
+import { TableClass } from 'aws-cdk-lib/aws-dynamodb';
+import { RetentionDays } from 'aws-cdk-lib/aws-logs';
+import { PipelineStackProps } from '../lib/pipeline-stack';
+
+export const devProps: PipelineStackProps = {
+  trustedAccount: '',
+  stage: 'dev',
+  lambdaFunctionConfigs: {
+    memorySize: 128,
+    ephemeralStorageSize: 512,
+    logRetention: RetentionDays.ONE_DAY,
+  },
+  apiGatewayConfigs: {
+    defaultCorsPreflightOptions: {
+      allowOrigins: ['foo.com', 'api.foo.com'],
+      allowMethods: ['OPTIONS', 'GET', 'POST', 'PUT', 'DELETE'],
+      allowHeaders: ['Content-Type'],
+    },
+    enableRateLimitApi: true,
+    apiRequestLimit: 3000,
+    apiRequestLimitPeriod: Period.WEEK,
+  },
+  databaseConfigs: {
+    tableName: 'notes',
+    tableClass: TableClass.STANDARD,
+    writeCapacity: 5,
+    readCapacity: 5,
+    replicationRegions: [],
+    removalPolicy: RemovalPolicy.DESTROY,
+  },
+  env: {
+    account: '',
+    region: 'ap-southeast-1',
+  },
+};
